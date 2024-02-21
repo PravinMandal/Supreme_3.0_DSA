60a0c79598bc0d215ec394e09e5eab6b79566163
PravinMandal <PravinMandalp45@gmail.com>
2024-02-21 11:49:39 +0530

    first

---
+++ udemy/45FindingMaximumof2Numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"Enter two numbers";
+    cin>>x>>y;
+    if(x>y)
+    {
+        cout<<"Max number is"<<x;
+    
+    }
+    else
+    {
+        cout<<"Max number is "<<y;
+    }
+    return 0;
+
+}
