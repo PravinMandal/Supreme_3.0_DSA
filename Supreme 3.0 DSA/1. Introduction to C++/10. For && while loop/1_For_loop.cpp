6232a928e1be6ce6d97abfb4e034cdc02f1e21db
pravin <pravinmandalp45@gmail.com>
2024-04-27 15:49:30 +0530

    for loop and while loop

---
+++ Supreme 3.0 DSA/1. Introduction to C++/10. For && while loop/1_For_loop.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+{
+    // Homework Questions
+
+
+    // 1 to 100 counting
+
+    int i;
+
+    for (i = 1; i <= 100; i = i + 1)
+    {
+        cout << i << " ";
+    }
+    cout<<endl;
+}    
+
+
+{
+    // 100 to 1 counting
+
+    int i;
+
+    for(i=100;i>=1;i=i-1)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+}
+
+{
+     
+    //  50 times name
+
+    int i;
+
+    for(i=1;i<=50;i=i+1)
+    {
+        cout<<"Mai Pravin HU "<<endl;
+    }
+    cout<<endl;
+
+}
+
+{
+    
+    // 0 to -10 counting
+
+    int i;
+
+    for(i=0;i>=-10;i=i-1)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+}
+
+{
+
+    // 7 ka table
+
+    int i;
+
+    for(i=7;i<=70;i=i+7)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+}
+
+
+{
+
+    // A to Z alphabet
+
+    char i;
+
+    for(i='A';i<='Z';i=i+1)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+}
+
+{
+
+    // a to z alphabet
+
+    char i;
+
+    for(i='a';i<='z';i=i+1)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+}
+
+    return 0;
+}
