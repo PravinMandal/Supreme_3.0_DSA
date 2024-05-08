9464d50489c4bb840d0d7b68aba83cc8085636da
pravin <pravinmandalp45@gmail.com>
2024-05-08 23:56:46 +0530

    patterns

---
+++ Supreme 3.0 DSA/2. Basic of Programming/5. Patterns/1_Square_Pattern.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int side;
+    cout<<
+
+    for(int row=0; row<4 ; row++)
+    {
+
+        for(int col=0; col<4 ; col++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
