31ac4066cdfc6eea29244d004d7ccc5a241203a9
pravin <pravinmandalp45@gmail.com>
2024-05-09 02:09:32 +0530

    pattern

--- Supreme 3.0 DSA/2. Basic of Programming/5. Patterns/6_Inverse_Half_Pyramid.cpp
+++ Supreme 3.0 DSA/2. Basic of Programming/5. Patterns/6_Inverse_Half_Pyramid.cpp
@@ -9,12 +9,13 @@ int main()
 
     for(int row=0 ; row<n ; row++)
     {
-        for(int col=0 ; col<row+1 ; col++)
+        for(int col=0 ; col< n-row  ; col++)
         {
             cout<<"* ";
         }
         cout<<endl;
     }
+
 
 
 
---
+++ Supreme 3.0 DSA/2. Basic of Programming/5. Patterns/7_Hollow_Half_Pyaramid.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter Length of Pyramid :-";
+    cin>>n;
+
+    for(int row=0 ; row<n ; row++)
+    {
+        for(int col=0 ; col<row+1 ; col++)
+        {
+            if(row==0 || row== n-1 || col==0|| row==col )
+            {
+                cout<<"* ";      
+            }
+            else
+            {
+                cout<<"  ";
+            }
+
+            // if( row==0 || row==1|| row == n-1)
+            // {
+            //     cout<<"* ";
+            // }
+            // else
+            // {
+            //     if(col==0 || col==row)
+            //     {
+            //         cout<<"* ";
+
+            //     }
+            //     else
+            //     {
+            //         cout<<"  ";
+            //     }
+            // }
+
+
+        }
+        cout<<endl;
+    }
+
+
+
+
+    return 0;
+}
