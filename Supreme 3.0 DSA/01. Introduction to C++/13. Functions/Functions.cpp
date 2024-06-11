128a11f386ea8d40656943e30b9df2001ee71026
Pravin Mandal <pravinmandalp45@gmail.com>
2024-06-11 23:25:13 +0530

    silly

--- Supreme 3.0 DSA/01. Introduction to C++/13. Functions/Functions.cpp
+++ Supreme 3.0 DSA/01. Introduction to C++/13. Functions/Functions.cpp
@@ -1,80 +1,48 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
-
-
 
 void printcounting()
 {
-    for( int i=1;i<=100;i++)
+    for (int i = 1; i <= 100; i++)
     {
-        cout<<i<<" ";
+        cout << i << " ";
     }
-    
 }
-
 
 int simpleinterest(int p, int r, int t)
 {
-    int SI = (p * r * t)/100 ;
+    int SI = (p * r * t) / 100;
     return SI;
 }
 
 void votechecker(int age)
 {
-    
-    if ( age<=18)
+
+    if (age <= 18)
     {
-        cout<<" Not eligible for voting ";
+        cout << " Not eligible for voting ";
     }
     else
     {
-        cout<<"Eligible for voting ";
+        cout << "Eligible for voting ";
     }
 }
 
-// M is the amount you receive upon maturity.
-// P is the amount you invest at regular intervals.
-// n is the number of payments you have made.
-// i is the periodic rate of interest.
-
-// M = P × ({[1 + i]^n – 1} / i) × (1 + i).
-
-
-
-double sipcalculator( int p ,int n, int i)
-{
-   double m= p*((pow(1+i, n)-1)/i)*(1+i);
-   return m;
-}
-
-
-
 int main()
 {
- printcounting();
+    printcounting();
 
- cout<<endl;
+    cout << endl;
 
- int answerofsimpleinterest=simpleinterest (5000, 12 ,2);
- cout<< answerofsimpleinterest;
+    int answerofsimpleinterest = simpleinterest(5000, 12, 2);
+    cout << answerofsimpleinterest;
 
- cout<<endl;
+    cout << endl;
 
+    votechecker(17);
 
+    cout << endl;
 
-votechecker(17);
-
-cout<<endl;
-
-
-cout<<sipcalculator(500,10, 12);
-// cout<< maturityAmount;
-
-
-
-
-
-
-return 0;
+    return 0;
 }
