1f9ef96ca944cd1db47d81ba432c6e112f2eb715
Pravin Mandal <pravinmandalp45@gmail.com>
2024-06-13 20:50:50 +0530

    nothing

--- Supreme 3.0 DSA/02. Basic of Programming/1. Code Testing/1_Code_Testing.cpp
+++ Supreme 3.0 DSA/02. Basic of Programming/1. Code Testing/1_Code_Testing.cpp
@@ -3,14 +3,25 @@ using namespace std;
 
 int main()
 {
-    // int a=10 ;
-    // cout<< (++a) * (++a)<<endl;
-    // cout<< (++a) * (a++)<<endl;
-    // cout<< (a++) * (++a)<<endl;
-    // cout<< (a++) * (a++)<<endl;
-
-
-    for()
+    for( int row=0 ; row< 5 ; row++)
+    {
+        for( int col=0 ; col< 5 ; col++)
+        {
+            if(row==0 || row== 4)
+            {
+                cout<<"1 ";
+            }
+            else
+            {
+                if(col==0 || col==4)
+                cout<<"2 ";
+                else
+                cout<<"  ";
+            
+            }
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
