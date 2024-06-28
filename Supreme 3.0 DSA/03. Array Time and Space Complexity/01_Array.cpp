40da80b17437aaae7d418e650a8d109c7a59da6a
Pravin Mandal <pravinmandalp45@gmail.com>
2024-06-29 00:23:33 +0530

    array

--- Supreme 3.0 DSA/03. Array Time and Space Complexity/01_Array.cpp
+++ Supreme 3.0 DSA/03. Array Time and Space Complexity/01_Array.cpp
@@ -1,11 +1,39 @@
 #include<iostream>
 using namespace std;
 
+bool findtarget(int arr[] , int size ,int targetValue )
+{
+    
+    for(int i=0 ; i<size ; i++)
+    {
+       if (arr[i]==targetValue)
+       {
+        return true;
+       }
+
+    }
+    return false;
+}
 
 int main ()
 {
+    int size;
+    int arr[50];
+    cout<<"Enter number of element: ";
+    cin>>size;
 
-    for(int i=0)
+    for(int i=0 ; i<size ; i++)
+    {
+        cout<<"Enter the input value for index "<<i << endl;
+        cin>> arr[i];
+
+    }
+    int targetValue;
+    cout <<"Enter target value: ";
+    cin>>targetValue;
+    findtarget(arr, size , targetValue);
+    bool ans= findtarget (arr , size , targetValue) ;
+    cout<<ans;
 
 
 
