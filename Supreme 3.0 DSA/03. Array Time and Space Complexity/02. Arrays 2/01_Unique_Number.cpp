900d466cfb9551416f3bdb5c98768c4122ad0d0d
Pravin Mandal <pravinmandalp45@gmail.com>
2024-07-15 14:45:01 +0530

    unique number

---
+++ Supreme 3.0 DSA/03. Array Time and Space Complexity/02. Arrays 2/01_Unique_Number.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int uniquenumber( int arr[] , int n)
+{
+    int ans=0;
+    for(int i=0 ;i<n ; i++)
+    {
+         ans= ans^arr[i];
+    }
+    return ans;
+}
+
+int main()
+{
+    int arr[]={2,2,3,3,4};
+    int size=5;
+
+    
+    int ans= uniquenumber(arr , size);
+    cout<<"Unique number is : "<<ans;
+    
+
+
+
+    return 0;
+}
