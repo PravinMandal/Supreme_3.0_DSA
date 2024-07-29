c268a87f4512e8f6d16cd6f89f45884e78773d95
Pravin Mandal <pravinmandalp45@gmail.com>
2024-07-29 14:55:31 +0530

    2d array

--- Supreme 3.0 DSA/03. Array Time and Space Complexity/04.Arrays 3/01_2D_Array.cpp
+++ Supreme 3.0 DSA/03. Array Time and Space Complexity/04.Arrays 3/01_2D_Array.cpp
@@ -1,17 +1,8 @@
 #include<iostream>
 using namespace std;
 
-int main()
+int ToPrintRightDiagonals(int arr[] [3] , int n)
 {
-    int n=3;
-    int arr [3] [3]={
-        {1,2,3},
-        {4,5,6},
-        {7,8,9}
-    };
-
-
-
     for(int i=0 ; i<n ; i++)
     {
         for(int j=0 ; j<n ; j++)
@@ -23,6 +14,32 @@ int main()
         }
         cout<<endl;
     }
+    return 0;
+}
+
+int main()
+{
+    int n=3;
+    int arr [3] [3]={
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    ToPrintRightDiagonals(arr,n);
+
+    // for(int i=0 ; i<n ; i++)
+    // {
+    //     for(int j=0 ; j<n ; j++)
+    //     {
+    //         if(j==n-i-1)
+    //         {
+    //             cout<< arr[i][j];     
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
 
     return 0;
 }
