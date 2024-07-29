a372cef3475ef6548400af62ab0530d78887f1ed
Pravin Mandal <pravinmandalp45@gmail.com>
2024-07-29 16:20:18 +0530

    max and min in 2d array

--- Supreme 3.0 DSA/03. Array Time and Space Complexity/04.Arrays 3/02_Search2DArray.cpp
+++ Supreme 3.0 DSA/03. Array Time and Space Complexity/04.Arrays 3/02_Search2DArray.cpp
@@ -18,22 +18,59 @@ bool find2Darray(int arr[][4] , int n , int m , int target)
     return false;
 }
 
+int FindMinInArray( int arr[][4] ,int rowsize , int columnsize)
+{
+    int minvalue=INT8_MAX;
+    for(int i=0 ; i<rowsize ; i++)
+    {
+        for(int j=0 ; j<columnsize ; j++)
+        {
+            minvalue = min(arr[i][j] , minvalue);
+        }
+    }
+    return minvalue;
+}
+
+int FindMaxInArray( int arr[][4] , int rowsize , int columnsize)
+{
+    int maxvalue=INT8_MIN;
+    for(int i=0 ; i<rowsize ; i++)
+    {
+        for(int j=0 ; j<columnsize ; j++)
+        {
+            maxvalue = max(arr[i][j] , maxvalue);
+        }
+    }
+    return maxvalue;
+
+}
 
 int main()
 {
     int arr[3][4]=
     {
         {10,11,12,13},
-        {20,21,22,23},
+        {20,21,22,-2},
         {30,31,41,55}
     };
 
     int rowsize = 3;
     int columnsize=4;
-    int target=55;
 
-    bool ans=find2Darray(arr,rowsize,columnsize,target);
-    cout<<"Ans is: "<<ans;
+    int Min=FindMinInArray(arr, rowsize, columnsize);
+    cout<<"Min number is: "<<Min;
+
+    cout<<endl;
+
+    int Max=FindMaxInArray(arr, rowsize, columnsize);
+    cout<<"Max number is: "<<Max;
+
+
+
+    // int target=55;
+
+    // bool ans=find2Darray(arr,rowsize,columnsize,target);
+    // cout<<"Ans is: "<<ans;
 
     return 0;
 }
