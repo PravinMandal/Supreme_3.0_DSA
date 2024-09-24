08e49d797bc83e81aed6f522c01ffc5a48cfa998
Pravin Mandal <pravinmandalp45@gmail.com>
2024-09-24 18:24:00 +0530

    string basics

---
+++ Supreme 3.0 DSA/07. Char array & Strings/01. Lec 1/02. String/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+ // string sentence;
+    // cout<<"Enter your Name: ";
+    // getline(cin, sentence, 'A'); // 'A' delimeter h ye aate hi wo line cin krna band kr dega
+    // //cin.getline hum char mai use krte hai but 
+    // //'getline' design kiya gya h strings ke sath kaam krne ke liye
+    // cout<< sentence;
