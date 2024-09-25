5107d06d4e23405d7d5d1677f7613389eddf0415
Pravin Mandal <pravinmandalp45@gmail.com>
2024-09-26 03:51:28 +0530

    count palindromic substring

---
+++ Supreme 3.0 DSA/07. Char array & Strings/02. Lec 2/04_Count_Palindromic_Substrings.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int expandAroundCentre(string s,int i,int j)
+{
+    int count=0;
+    while(s[i]==s[j] && i>=0 && j<s.length())
+    {
+        count++;
+        i--;
+        j++;
+    }
+    return count;
+}
+
+
+int countSubstring(string s)
+{
+    int totalcount=0;
+    for(int centre=0 ; centre<s.length() ; centre++ )
+    {
+        //odd
+        int i=centre;
+        int j=centre;
+        int oddPalSubStringkaCount=expandAroundCentre(s,i,j);
+        //even
+        i=centre;
+        j=centre+1;
+        int evenPalSubStringkaCount=expandAroundCentre(s,i,j);
+        totalcount=totalcount+oddPalSubStringkaCount+evenPalSubStringkaCount;
+    }
+    return totalcount;
+}
+
+
+int main()
+{   
+    string s="babbar";
+    int ans=countSubstring(s);
+    cout<<"Number of palindrome Substring: "<<ans;
+    
+
+
+    return 0;
+}
