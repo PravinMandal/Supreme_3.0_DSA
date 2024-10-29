6bd88582019b12ef4d07259de9acc6165c11bfe3
Pravin Mandal <pravinmandalp45@gmail.com>
2024-10-29 12:42:19 +0530

    array unique number

--- Supreme 3.0 DSA/03. Array Time and Space Complexity/02. Arrays 2/01_Unique_Number.cpp
+++ Supreme 3.0 DSA/03. Array Time and Space Complexity/02. Arrays 2/01_Unique_Number.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 int uniquenumber( int arr[] , int n)
@@ -11,6 +13,29 @@ int uniquenumber( int arr[] , int n)
     return ans;
 }
 
+
+//method 2
+
+int uniquenumber2(vector<int>& brr){ //2,2,3,4,4
+
+    sort(brr.begin() , brr.end());
+    int n=brr.size();
+    int ans=-1;
+    int i=0;
+    while(i<n){
+        if(brr[i]==brr[i+1] && (i+1<n))
+        {
+            i=i+2;
+        }
+        else{
+            ans=brr[i];
+            return ans;
+        }
+
+    }
+    return ans;
+}
+
 int main()
 {
     int arr[]={2,2,3,4,4};
@@ -18,7 +43,22 @@ int main()
 
     
     int ans= uniquenumber(arr , size);
-    cout<<"Unique number is : "<<ans;
+    cout<<"Unique number is : "<<ans<<endl;
+
+
+    //method 2
+    vector<int>brr;
+
+    brr.push_back(2);
+    brr.push_back(2);
+    brr.push_back(3);
+    brr.push_back(4);
+    brr.push_back(4);
+
+    int ans2= uniquenumber2(brr);
+    cout<<"Unique number is : "<<ans2<<endl;
+
+
     
 
 
