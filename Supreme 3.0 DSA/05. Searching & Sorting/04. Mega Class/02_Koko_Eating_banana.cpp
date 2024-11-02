d6f58c4e92c937cd2e17110b2abf6faa3dddd341
Pravin Mandal <pravinmandalp45@gmail.com>
2024-11-02 21:48:05 +0530

    koko eating bananas

--- Supreme 3.0 DSA/05. Searching & Sorting/04. Mega Class/02_Koko_Eating_banana.cpp
+++ Supreme 3.0 DSA/05. Searching & Sorting/04. Mega Class/02_Koko_Eating_banana.cpp
@@ -10,10 +10,53 @@
 
 #include<iostream>
 #include<vector>
+#include<algorithm>
+#include<cmath>
 using namespace std;
 
+bool CanKokoFinishBananasWithKspeed(vector<int>&piles,int h , int k){
+    long long int TotalHoursTakenbyKokoToFinishAllBananas=0;
+    for(int i=0 ; i<piles.size() ; i++){
+        TotalHoursTakenbyKokoToFinishAllBananas += ceil(piles[i]/(double)k); 
+        //k int tha toh typecast kr denge double mai tbhi ceil kaam krega kyuki ceil ke liye decimal chahiye toh hoga na but int nhi krta wo
+
+        //ceil mtlb round off krke uska next number if decimal mai h
+        // ceil(10/3)=3.33 = 4
+
+        //floor mai niche wala le lega
+        //floor(10/3)=3.33 =3
+    }
+    return TotalHoursTakenbyKokoToFinishAllBananas<=h;
+    //agar condition satisfy hua then true jayega warna false 
+}
+
+int MinEatingSpeed(vector<int>& piles , int h){
+    int s=1;
+    auto it=max_element(piles.begin() , piles.end());
+    int e=*it;
+    int ans=-1;
+    while(s<=e){
+        int mid=s+((e-s)>>1);
+        int k=mid;
+        if(CanKokoFinishBananasWithKspeed(piles,h,k)){
+            ans=k;
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+    }
+    return ans;
+}
 
 int main(){
+
+    vector<int> piles={3,6,7,11};
+    int h=8;
+    //hume k find krna hai
+    int k=MinEatingSpeed(piles,h);
+    cout<<"Min eating speed to eat all the banana's: "<<k<<endl;
+
 
 
 
