516cd10aa5d96ade6bae32313ce0576029fab785
Pravin Mandal <pravinmandalp45@gmail.com>
2024-11-07 01:09:17 +0530

    array is sorted or not

---
+++ Supreme 3.0 DSA/09. Recursion/02. Lec 2/07_Array_is_Sorted_or_Not.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+bool ArrayisSortedOrNot(int arr[], int size , int index){
+    if(index==size){
+        //last tak aa gya bina false return hue
+        //array sorted hai 
+        return true;
+    }
+    if(arr[index]>arr[index+1] && (index+1)< size)
+    {
+        //aage wala element chhota h 
+        //sorted nhi h
+        return false;
+    }
+    bool ans=ArrayisSortedOrNot(arr,size,index+1);
+}
+
+int main(){
+
+    int arr[]={10,20,30,40,70,60};
+    int size=6;
+    int index=0;
+
+    bool ans=ArrayisSortedOrNot(arr,size,index);
+    cout<<ans<<endl;
+
+    return 0;
+}
