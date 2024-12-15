d18b90a713d80a3294ed1f98ef05c54292042acb
Pravin Mandal <pravinmandalp45@gmail.com>
2024-12-15 22:50:29 +0530

    vector basics

--- 03. Array Time and Space Complexity/03. vector STL in c++/01_vector.cpp
+++ 03. Array Time and Space Complexity/03. vector STL in c++/01_vector.cpp
@@ -17,12 +17,15 @@ int main()
     //Just keep inserting it will manage by itself
 
     vector<int> v; //array hi hai
+    vector<int> v2={1,2,3,4,5}; // aise bhi kr skte h
 
     v.push_back(1);
     v.push_back(2);
     v.push_back(3);
 
     print(v);
+    cout<<endl;
+    print(v2);
 
 
     return 0;
