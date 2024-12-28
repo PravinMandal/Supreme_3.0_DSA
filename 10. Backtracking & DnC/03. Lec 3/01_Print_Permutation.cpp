1ae4679e1ad3cee4c98a6122aaefcc89755ac223
Pravin Mandal <pravinmandalp45@gmail.com>
2024-12-29 02:07:25 +0530

    print permutaion

---
+++ 10. Backtracking & DnC/03. Lec 3/01_Print_Permutation.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void PrintPermutaion(string &str, int i){
+    //base case
+    if(i>=str.length()){
+        cout<<str<<endl;
+        return;
+    }
+
+    //mai i wale case pr sbko chance dunga
+    for(int j=i ; j<str.length() ; j++){
+        //swap krne pr wo position pr sbko mauka milega
+        swap(str[i], str[j]);
+        //recursion samhal lega aage ka
+        PrintPermutaion(str,i+1);
+        //backtracking , jo change kiya h usse wapas pehle jaise banana h
+        swap(str[i], str[j]);
+    }
+}
+
+int main(){
+    string str="abc";
+    int i=0;
+    PrintPermutaion(str,i);
+}
