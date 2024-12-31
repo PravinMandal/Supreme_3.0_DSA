3d1ca36429710a38dde11e88dd42c3ea6d572725
Pravin Mandal <pravinmandalp45@gmail.com>
2024-12-31 14:34:59 +0530

    missing number

---
+++ 03. Array Time and Space Complexity/06. Assignments/03_Missing_Number.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<numeric>
+using namespace std;
+
+class Solution{
+public:
+    int missingNumber(vector<int>&nums){
+         int n=nums.size();
+        unordered_map<int,int>mapping;
+        for(int i=0;i<n; i++){
+            mapping[nums[i]]=i;
+        }
+        for(int i=0; i<=n; i++){
+            if(mapping.find(i)!=mapping.end()){
+
+            }
+            else{
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //ye sabse mst hai
+    int missingNumber2(vector<int>& nums) {
+        int n=nums.size();
+        int num=(n*(n+1))/2;
+        int sum=accumulate(nums.begin(), nums.end(), 0);
+        return num-sum;
+    }
+};
+
+int main(){
+
+    vector<int>nums={0,1,2,3,5,6,7,8,9};
+    Solution sol;
+    int ans=sol.missingNumber2(nums);
+
+    if(ans!=-1){
+        cout<<"the missing number is: "<<ans<<endl;
+    }
+
+    return 0;
+}
