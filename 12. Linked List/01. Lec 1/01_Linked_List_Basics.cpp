7162aaf49727cb08560c4d6300b52db22c022f24
Pravin Mandal <pravinmandalp45@gmail.com>
2025-01-02 23:30:58 +0530

    Linked list basics delete from LL

--- 12. Linked List/01. Lec 1/01_Linked_List_Basics.cpp
+++ 12. Linked List/01. Lec 1/01_Linked_List_Basics.cpp
@@ -84,6 +84,99 @@ void insertatPosition(int position, int value, Node* &head, Node* &tail){
     }
 }
 
+int SearchInLL(int target,Node* &head,Node* &tail){
+    int length=1;
+    Node* temp=head;
+    while(temp!=NULL){
+        if(temp->data==target){
+            return length;
+        }
+        length++;
+        temp=temp->next;
+    }
+    //yaha tak aa gye mtlb target nhi mila
+    return -1;
+}
+
+
+void DeleteFromLL(int position,Node* &head, Node* &tail){
+    //pehle check kro kya pta koi node bana hi na ho
+    if(head==NULL && tail==NULL){
+        cout<<"there is not even a single node to delete "<<endl;
+    }
+    
+    //if sirf ek hi LL node ho
+    if(head==tail){
+        head=NULL;
+        tail=NULL;
+    }
+    else{
+        //multiple nodes
+        // isme bhi 2 cases honge 
+        //case 1: 1st node delete krna h
+        if(position==1){
+            Node* temp=head; //ek temp bana liye taaki uske through delete kre
+            head=temp->next; // ab head next wale pr chala gya 
+            temp->next=NULL;
+            delete temp; //jo nodes bane hote h wo heap mai banaya h humne toh usse delete krna zaruri h
+        }
+        else{
+            //other then 1st position delete krna h
+            Node* prev=head;
+            // ab temp ko aage badhakr jisse delete krna h uske piche wale pr point kr denge
+            for(int i=0; i<position-2; i++){
+                prev=prev->next; 
+            }
+            Node* curr=prev->next;
+            Node* forward=curr->next;
+            prev->next=forward;
+            curr->next=NULL;
+            delete curr;
+        }
+    }
+
+}
+
+// isme position nhi user bolega ye element delete krna h usko delete krna pdega
+void DeleteTargetFromLL(int target,Node* &head, Node* &tail){
+    int position=SearchInLL(target,head,tail); // ye mujhe position laake de dega
+    //pehle check kro kya pta koi node bana hi na ho
+    if(head==NULL && tail==NULL){
+        cout<<"there is not even a single node to delete "<<endl;
+    }
+    
+    //if sirf ek hi LL node ho
+    if(head==tail){
+        head=NULL;
+        tail=NULL;
+    }
+    else{
+        //multiple nodes
+        // isme bhi 2 cases honge 
+        //case 1: 1st node delete krna h
+        if(position==1){
+            Node* temp=head; //ek temp bana liye taaki uske through delete kre
+            head=temp->next; // ab head next wale pr chala gya 
+            temp->next=NULL;
+            delete temp; //jo nodes bane hote h wo heap mai banaya h humne toh usse delete krna zaruri h
+        }
+        else{
+            //other then 1st position delete krna h
+            Node* prev=head;
+            // ab temp ko aage badhakr jisse delete krna h uske piche wale pr point kr denge
+            for(int i=0; i<position-2; i++){
+                prev=prev->next; 
+            }
+            Node* curr=prev->next;
+            Node* forward=curr->next;
+            prev->next=forward;
+            curr->next=NULL;
+            delete curr;
+        }
+    }
+
+}
+
 void print(Node* head){
     Node* temp=head; //temp ko head se intialize kr diye
     //ab head ko thodi idhr udhr kr skte h warna head lost ho jayega isiliye copy kr diye temp mai
@@ -114,15 +207,34 @@ int main(){
     head=insertatTail(50,head,tail);
     print(head);
     // 30->20->10->40->50->NULL
-    insertatPosition(1,50,head,tail);
+    insertatPosition(1,55,head,tail);
     print(head);
-    // 50->30->20->10->40->50->NULL
+    // 55->30->20->10->40->50->NULL
     insertatPosition(7,90,head,tail);
     print(head);
-    // 50->30->20->10->40->50->90->NULL
+    // 55->30->20->10->40->50->90->NULL
     insertatPosition(3,69,head,tail);
     print(head);
-    // 50->30->69->20->10->40->50->90->NULL
+    // 55->30->69->20->10->40->50->90->NULL
+    int search=SearchInLL(10,head,tail);
+    if(search==-1){
+        cout<<"target Not found"<<endl;
+    }
+    else{
+        cout<<"Target found at position "<<search<<endl;
+    }
+    DeleteFromLL(1,head,tail);
+    print(head);
+    // 30->69->20->10->40->50->90->NULL
+    DeleteFromLL(7,head,tail);
+    print(head);
+    // 30->69->20->10->40->50->NULL
+    DeleteFromLL(4,head,tail);
+    print(head);
+    // 30->69->20->40->50->NULL
+    DeleteTargetFromLL(69,head,tail);
+    print(head);
+    // 30->20->40->50->NULL
 
 
 
