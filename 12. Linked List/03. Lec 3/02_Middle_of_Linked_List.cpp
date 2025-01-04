b8fd4640c68a66d80889151812172e597ab8a667
Pravin Mandal <pravinmandalp45@gmail.com>
2025-01-04 19:25:06 +0530

    Middle node of linked list

--- 12. Linked List/03. Lec 3/02_Middle_of_Linked_List.cpp
+++ 12. Linked List/03. Lec 3/02_Middle_of_Linked_List.cpp
@@ -13,12 +13,15 @@ struct ListNode {
 
 class Solution {
 public:
-    ListNode* middleNode(ListNode* &head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast != NULL && fast->next != NULL) {
-            fast = fast->next->next;
-            slow = slow->next;
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL){
+            fast=fast->next;
+            if(fast!=NULL){
+                fast=fast->next;
+                slow=slow->next;
+            }
         }
         return slow;
     }
