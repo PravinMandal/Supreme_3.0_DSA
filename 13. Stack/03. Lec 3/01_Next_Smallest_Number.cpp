3faf3f5ff9d893a6c3b0298302be0b4b242c1927
Pravin Mandal <pravinmandalp45@gmail.com>
2025-01-17 17:51:02 +0530

    Next smallest number

--- 13. Stack/03. Lec 3/01_Next_Smallest_Number.cpp
+++ 13. Stack/03. Lec 3/01_Next_Smallest_Number.cpp
@@ -9,14 +9,15 @@ using namespace std;
 void findAns(vector<int>&arr, stack<int>&s, vector<int>&ans){
     int n=arr.size();
     for(int i=n-1; i>=0; i--){
-        while(s.top() >= arr[i]){
+        int element=arr[i];
+        while(s.top() >= element){
             //agar top wala element bada h the remove krdo kyuki jo abhi new chhota element h wo chhota h and jo aage aayenge
             //unke liye toh ye jo chhota wala h wo smallest hoga , naaki jisse pop kr rhe h wo kyuki wo comparitively bada h
             s.pop();
         }
         //yaha tak aa ya mtlb ab stack ke top pr jo element h wo element se chhota h
         ans.push_back(s.top());
-        s.push(arr[i]);
+        s.push(element);
     }
 }
 
@@ -24,7 +25,7 @@ int main(){
 
     vector<int>v={8,6,2,4,3};
     stack<int>s;
-    s.push(-1);
+    s.push(-1); //jo sbse last wala element hai uske right mai toh koi hoga hi nhi isiliye by default -1;
     vector<int>ans;
 
     findAns(v,s,ans);
