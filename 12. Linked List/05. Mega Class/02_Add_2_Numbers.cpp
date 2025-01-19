17f1b9a11d7f33cc5b1a70315853c80163da8477
Pravin Mandal <pravinmandalp45@gmail.com>
2025-01-19 20:28:12 +0530

    add 2 numbers in linked list recursive

--- 12. Linked List/05. Mega Class/02_Add_2_Numbers.cpp
+++ 12. Linked List/05. Mega Class/02_Add_2_Numbers.cpp
@@ -31,8 +31,24 @@ public:
         delete ans;
         return finalans;
     }
+
+    ListNode* recursive(ListNode* l1, ListNode* l2, int carry=0){
+        if(!l1 && !l2 && !carry) return 0;
+
+        int a = l1 ? l1->val : 0;
+        int b = l2 ? l2->val : 0;
+        int sum = a + b + carry;
+        int digit = sum % 10;
+        carry = sum / 10;
+
+        ListNode* ans=new ListNode(digit);
+        ans->next = recursive(l1 ? l1->next : l1 , l2 ? l2->next : l2, carry);
+        return ans;
+    }
+
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        return iterative(l1,l2);
+        // return iterative(l1,l2);
+        return recursive(l1, l2);
     }
 };
 
