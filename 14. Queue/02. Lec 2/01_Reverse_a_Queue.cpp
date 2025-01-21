37c1ee91affaeda17d4107abed5e06b6934ba8fc
Pravin Mandal <pravinmandalp45@gmail.com>
2025-01-21 11:56:57 +0530

    reverse a queue

---
+++ 14. Queue/02. Lec 2/01_Reverse_a_Queue.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+
+void reverseQueue(queue<int> &q){
+    //ab reverse krne ke liye hum stack ka use krenge
+    stack<int>st;
+
+    while(!q.empty()){
+        int element = q.front();
+        q.pop();
+        st.push(element);
+    }
+    //ab yaha tk queue ke saare element stack mai store ho chuka hai
+    //ab yaha se agar stack ke top ko excess krte krte pop kru toh mujhe reverse order mai elements milenge
+    while(!st.empty()){
+        int element = st.top();
+        st.pop();
+        q.push(element);
+    }
+}
+
+int main(){
+
+    queue<int>q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+
+    reverseQueue(q);
+
+    //printing
+    while(!q.empty()){
+        cout<<q.front()<<" ";
+        q.pop();
+    }cout<<endl;
+
+    return 0;
+}
