b73102d9f8c9bc891232a121a2ef810e6d895a37
Pravin Mandal <pravinmandalp45@gmail.com>
2025-01-29 21:37:09 +0530

    create tree

---
+++ 15. Generic & Binary Tree/01. Lec 01/01_Create_Tree.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value){
+        this->data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* createNode(){
+    cout << "Enter the value for Node: "<<endl;
+    int value;
+    cin >> value;
+
+    if(value == -1){
+        return NULL;
+    }
+    else{
+    Node* root = new Node(value);
+
+    //ab baaki kaam recursion se karwa lo
+    cout<<"adding left child for root "<<root->data<<endl;
+    root->left = createNode();
+
+    cout<<"adding right child for root "<<root->data<<endl;
+    root->right = createNode();
+    }
+}
+
+int main(){
+
+    Node* root;
+
+    root = createNode();
+
+
+    return 0;
+}
