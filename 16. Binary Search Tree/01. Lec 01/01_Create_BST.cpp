d384ba43af9157e3f0e0367d26ff9d13584a63e1
Pravin Mandal <pravinmandalp45@gmail.com>
2025-02-14 00:36:14 +0530

    BST traversal

--- 16. Binary Search Tree/01. Lec 01/01_Create_BST.cpp
+++ 16. Binary Search Tree/01. Lec 01/01_Create_BST.cpp
@@ -69,11 +69,45 @@ void levelorderTraversal(Node* root){
     }
 }
 
+void preorderTraversal(Node* root) {
+    if(root == NULL) return;
+    cout<<root->data<<" ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+    return;
+}
+
+void inorderTraversal(Node* root) {
+    if(root == NULL) return;
+    inorderTraversal(root->left);
+    cout<<root->data<<" ";
+    inorderTraversal(root->right);
+    return;
+}
+
+void postorderTraversal(Node* root) {
+    if(root == NULL) return;
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    cout<<root->data<<" ";
+    return;
+}
+
 int main() {
 
     Node* root = NULL;
-    createNode(root);
+    createNode(root); //100 50 200 70 20 250 150 -1
     levelorderTraversal(root);
+    cout<<endl<<"Preorder: ";
+    preorderTraversal(root);
+    cout<<endl<<"Inorder: ";
+    inorderTraversal(root);
+    cout<<endl<<"Postorder: ";
+    postorderTraversal(root);
+
+
+    //BST mai uska inorder traversal krenge toh humesha sorted hi aayega
+    //BST ko inorder mai traverse kro dry run and dekho sorted print hota h
 
     return 0;
 }
