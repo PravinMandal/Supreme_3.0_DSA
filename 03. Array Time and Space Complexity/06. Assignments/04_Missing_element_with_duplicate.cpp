26bb76d2aa733b9548626dfe7157711cc97ace47
Pravin Mandal <pravinmandalp45@gmail.com>
2025-03-07 20:22:30 +0530

    missing element of an array with duplicate

--- 03. Array Time and Space Complexity/06. Assignments/04_Missing_element_with_duplicate.cpp
+++ 03. Array Time and Space Complexity/06. Assignments/04_Missing_element_with_duplicate.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 using namespace std;
 
-void findMissingElement(vector<int>& arr) {
+void findMissingElement(vector<int> arr) {
     //visited method
     int n = arr.size();
     for(int i=0; i<n; i++){
@@ -23,11 +23,33 @@ void findMissingElement(vector<int>& arr) {
     }
 }
 
+void findMissingElement2(vector<int> arr) {
+    int n = arr.size();
+
+    for(int i=0; i<n; i++) {
+        int index = arr[i] -1;
+
+        if(arr[i] != arr[index]) {
+            swap(arr[i], arr[index]);
+        }
+        else{
+            ++i; 
+        }
+    }
+
+    cout<<"Missing Element is: ";
+    for(int i=0; i<n; i++) {
+        if(arr[i] != i+1) {
+            cout<<i+1<<" ";
+        }
+    }
+}
+
 int main() {
 
     vector<int>arr = {1,3,5,3,4};
 
-    findMissingElement(arr);
+    findMissingElement2(arr);
 
     return 0;
 }
