fb980bb3744ce3050d6b968e1e996a136177cd4b
Pravin Mandal <pravinmandalp45@gmail.com>
2025-07-09 22:35:56 +0530

    weighted graph

--- 20. Graph/01. Graph Level 1/01. Lec 1/02_weighted_graph.cpp
+++ 20. Graph/01. Graph Level 1/01. Lec 1/02_weighted_graph.cpp
@@ -1,32 +1,37 @@
 #include<iostream>
 #include<list>
 #include<unordered_map>
+#include<queue>
 using namespace std;
 
 class Graph {
 public:
-    unordered_map<int, list<int>> adjList;
+    //first int -> u
+    //second int -> v
+    //third int -> wt
+    unordered_map<int, list<pair<int,int>>> adjList;
 
-    addEdge(int u, int v, bool direction) {
+    addEdge(int u, int v, int weight, bool direction) {
         //direction == 0 -> undirected graph
         //direction == 1 -> directed graph
         if(direction == 0) {
             //Undirected edge
-            adjList[u].push_back(v);
-            adjList[v].push_back(u);
+            adjList[u].push_back({v, weight});
+            adjList[v].push_back({u, weight});
         }
         else {
             //directed edge
-            adjList[u].push_back(v);
+            adjList[u].push_back({v, weight});
         }
     }
 
     void printAdjList(int n) {
         for(int i=0; i<n; i++) {
             cout<<i<<": {";
-            list<int> temp = adjList[i];
+            list<pair<int,int>> temp = adjList[i];
             for(auto j : temp) {
-                cout<<j<<",";
+                //j ek pair hai
+                cout<<"("<<j.first<<","<<j.second<<")";
             }
             cout<<"}"<<endl;
         }
@@ -36,10 +41,11 @@ public:
 int main() {
 
     Graph g;
-    g.addEdge(0,1,1);
-    g.addEdge(0,2,1);
-    g.addEdge(1,2,1);
-    g.addEdge(2,3,1);
+    g.addEdge(0,1,3,1);
+    g.addEdge(0,2,4,1);
+    g.addEdge(1,2,1,1);
+    g.addEdge(2,3,16,1);
+
     g.printAdjList(4);
 
     return 0;
