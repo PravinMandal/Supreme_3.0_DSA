a3c15ea2e2098c47365ec75c27fb5fa63d5bdfbf
Pravin Mandal <pravinmandalp45@gmail.com>
2025-07-31 15:57:04 +0530

    173. Binary Search Tree Iterator

---
+++ 16. Binary Search Tree/04. Mega Class/03_BST_iterator.cpp
@@ -0,0 +1,105 @@
+//leetcode: 173. Binary Search Tree Iterator
+
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//Method 1: vector based
+class BSTIterator0 {
+public:
+    vector<int> inorder;
+    int i = 0;
+
+    void storeInorder(TreeNode* root) {
+        if(!root) return;
+        storeInorder(root->left);
+        inorder.push_back(root->val);
+        storeInorder(root->right);
+    }
+
+    BSTIterator0(TreeNode* root) {
+        storeInorder(root);
+    }
+
+    int next() {
+        return inorder[i++];
+    }
+
+    bool hasNext() {
+        return (i < inorder.size());
+    }
+};
+
+//Method 2 : recursive inorder traversal jaisa hi h but recursion ke jagah stack use kr rhe h
+class BSTIterator {
+    stack<TreeNode*> st;
+
+    void pushLeftNode(TreeNode* root) {
+        while(root) {
+            st.push(root);
+            root = root->left;
+        }
+    }
+
+public:
+    BSTIterator(TreeNode* root) {
+        pushLeftNode(root);
+    }
+
+    int next() {
+        auto top = st.top();
+        st.pop();
+
+        if(top->right) {
+            pushLeftNode(top->right);
+        }
+        return top->val;
+    }
+
+    bool hasNext() {
+        return !st.empty();
+    }
+};
+
+// Main function to test both BSTIterator0 and BSTIterator
+int main() {
+    /*
+        Constructing the following BST:
+                7
+               / \
+              3   15
+                 /  \
+                9    20
+    */
+
+    TreeNode* root = new TreeNode(7);
+    root->left = new TreeNode(3);
+    root->right = new TreeNode(15);
+    root->right->left = new TreeNode(9);
+    root->right->right = new TreeNode(20);
+
+    cout << "Using BSTIterator0 (vector based):\n";
+    BSTIterator0 it0(root);
+    while(it0.hasNext()) {
+        cout << it0.next() << " ";
+    }
+
+    cout << "\n\nUsing BSTIterator (stack based):\n";
+    BSTIterator it(root);
+    while(it.hasNext()) {
+        cout << it.next() << " ";
+    }
+
+    return 0;
+}
