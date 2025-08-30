22e4ff7c02f23edcd84278dcf905f913493ebbbf
Pravin Mandal <pravinmandalp45@gmail.com>
2025-08-30 23:18:23 +0530

    8. String to Integer (atoi)

--- 07. Char array & Strings/05. Assignment 5/10_String_to_Int.cpp
+++ 07. Char array & Strings/05. Assignment 5/10_String_to_Int.cpp
@@ -42,8 +42,30 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    int myAtoi(string s) {
+        int ans=0, i=0, sign=1;
+
+        while(s[i] == ' ') i++;
+        if(i<s.size() && (s[i] == '-' || s[i] == '+')) {
+            sign = s[i] == '+' ? 1 : -1;
+            i++;
+        }
+        while(i<s.size() && isdigit(s[i])) {
+            if(ans > INT_MAX/10 || (ans == INT_MAX/10 && s[i] > '7')) {
+                return sign == -1 ? INT_MIN : INT_MAX;
+            }
+            ans = ans * 10 + (s[i] - '0');
+            i++;
+        }
+        return ans*sign;
+    }
+};
+
+
 int main() {
-    Solution sol;
+    Solution2 sol;
 
     // Hardcoded testcases
     vector<string> testcases = {
