1da987b74c1ee33c15191f21d0d4b1c9f762f1e6
Pravin Mandal <pravinmandalp45@gmail.com>
2025-09-06 22:31:03 +0530

    spoj: PRATA

---
+++ 05. Searching & Sorting/05. Assignments/08_Spoj_Prata.cpp
@@ -0,0 +1,62 @@
+//spoj: PRATA - Roti Prata
+#include<bits/stdc++.h>
+using namespace std;
+ 
+bool isPossible(vector<int>& cookRank, int nP, int mid) {
+    int currP = 0; //initial cooked prata time
+    for(int i=0; i<cookRank.size(); i++) {
+        int R = cookRank[i], j=1;
+        int timeTaken = 0;
+        while(true) {
+            if(timeTaken +(j*R) <= mid) {
+                currP++;
+                timeTaken += j*R;
+                j++;
+            }
+            else {
+                break;
+            }
+        }
+        if(currP >= nP) {
+            return true;
+        }
+    }
+    return false;
+}
+ 
+int minTimeToCompleteOrder(vector<int>& cookRank, int nP) {
+    int s = 0;
+    int higestRank = *max_element(cookRank.begin(), cookRank.end());
+    int e = higestRank * (nP * (nP+1)>>1);
+    int ans = -1;
+    
+    while(s<=e) {
+        int mid = s + ((e-s)>>1);
+        if(isPossible(cookRank, nP, mid)) {
+            //mid time mai ho gya toh isse zyade time mai bhi ho jayega
+            ans = mid;
+            e = mid-1;
+        }
+        else {
+            s = mid+1;
+        }
+    }
+    return ans;
+}
+ 
+int main() {
+    // Hardcoded testcase
+    int T = 1;
+    cout << "Testcase: " << T << endl;
+    
+    // Example testcase from SPOJ PRATA:
+    // 1
+    // 10 4
+    // 1 2 3 4
+    int nP = 10, nC = 4;
+    vector<int> cookRank = {1, 2, 3, 4};
+    
+    cout << minTimeToCompleteOrder(cookRank, nP) << endl;
+    
+    return 0;
+}
