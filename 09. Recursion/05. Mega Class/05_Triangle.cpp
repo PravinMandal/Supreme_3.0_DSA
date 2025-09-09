6a63b5cdeed157faedf30919b73ee28c7e70304a
Pravin Mandal <pravinmandalp45@gmail.com>
2025-09-09 23:40:37 +0530

    120. Triangle

--- 09. Recursion/05. Mega Class/05_Triangle.cpp
+++ 09. Recursion/05. Mega Class/05_Triangle.cpp
@@ -19,12 +19,52 @@ int solve(vector<vector<int>>&triangle,int row, int col){
     return min(down,downPlus);
 }
 
-int minimumTotal(vector<vector<int>>&triangle){
-    int row=0;
-    int col=0;
+int solveMem(vector<vector<int>>& triangle, int row, int col, vector<vector<int>>& dp) {
+        if(row == triangle.size()-1) 
+            return triangle[row][col];
 
-    return solve(triangle,row,col);
-}
+        if(dp[row][col] != -1) {
+            return dp[row][col];
+        }
+
+        int down = triangle[row][col] + solveMem(triangle, row+1, col, dp);
+        int downplus = triangle[row][col] + solveMem(triangle, row+1, col+1, dp);
+        dp[row][col] = min(down, downplus);
+        return dp[row][col];
+    }
+
+    int solveTab(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        for(int col=0; col<n; col++) {
+            dp[n-1][col] = triangle[n-1][col];
+        }
+
+        for(int row=n-2; row>=0; row--) {
+            for(int col=row; col>=0; col--) {
+                int down = triangle[row][col] + dp[row+1][col];
+                int downplus = triangle[row][col] + dp[row+1][col+1];
+                dp[row][col] = min(down, downplus);
+            }
+        }
+        return dp[0][0];
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int row = 0;
+        int col = 0;
+        // int ans = solve(triangle, row, col);
+
+        //Memoisation
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        // int ans = solveMem(triangle, row, col, dp);
+
+        //Tabulation
+        int ans = solveTab(triangle);
+        return ans;
+    }
 
 int main(){
 
