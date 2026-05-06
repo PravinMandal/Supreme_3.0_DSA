e365d67d5e7a389d9dbf3abc7a647747158f6c02
Pravin Mandal <pravinmandalp45@gmail.com>
2026-05-07 01:18:33 +0530

    Shortest Path in Undirected Graph

--- 20. Graph/02. Graph Level 2/01. Lec 1/03_Shortest_Path_in_Undirected_Graph.cpp
+++ 20. Graph/02. Graph Level 2/01. Lec 1/03_Shortest_Path_in_Undirected_Graph.cpp
@@ -1,10 +1,83 @@
 //gfg: Shortest Path in Undirected Graph
-#include <iostream>
-#include <vector>
-#include <queue>
+#include<bits/stdc++.h>
 using namespace std;
 
 class Solution {
+  public:
+    vector<int> shortestPath(int V, vector<vector<int>> &edges, int src) {
+        // code here
+
+        // Adjacency list representation of graph
+        unordered_map<int, vector<int>> adj;
+
+        // Build undirected graph
+        for(auto edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        unordered_map<int, bool> visited;
+        unordered_map<int, int> parent;
+        
+        queue<int> q;
+
+        // Start BFS from source node
+        q.push(src);
+        visited[src] = true;
+        parent[src] = -1;
+        
+        // Standard BFS traversal
+        while(!q.empty()) {
+
+            auto frontNode = q.front(); 
+            q.pop();
+
+            // Visit all neighbours
+            for(auto nbr : adj[frontNode]) {
+
+                if(!visited[nbr]) {
+                    visited[nbr] = true;
+
+                    // Store parent for path reconstruction
+                    parent[nbr] = frontNode;
+
+                    q.push(nbr);
+                }
+            }
+        }
+        
+        vector<int> ans(V, -1);
+
+        // Calculate shortest distance using parent chain
+        for(int i=0; i<V; i++) {
+
+            // If node is unreachable and not source, skip
+            if(i != src && parent.find(i) == parent.end()) {
+                ans[i] = -1;
+                continue;
+            }
+
+            int node = i;
+            int count = 0;
+
+            // Move backwards using parent array
+            while(node != -1) {
+                node = parent[node];
+                count++;
+            }
+
+            ans[i] = count-1; // Distance from source
+        }
+
+        return ans;
+    }
+};
+
+
+class Solution2 {
   public:
     // Function to find the shortest path from source to all other nodes
     vector<int> shortestPath(vector<vector<int>>& adj, int src) {
@@ -33,7 +106,7 @@ class Solution {
 
 //main function for testing
 int main() {
-    Solution sol;
+    Solution2 sol;
 
     // Sample graph (undirected)
     // 0 -- 1
